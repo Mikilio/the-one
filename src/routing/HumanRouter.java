c55612713900d3bcdd7383e6b975152e6a9cb789
Mikilio <official.mikilio@gmail.com>
2025-06-02 15:32:25 +0200

    feat: complete the infection behavior
    - Slight modificationis to Seminar room for testing
    - Add proper pauses
    - Improve performance by reducing logic (using Passive Routers)

--- src/routing/HumanRouter.java
+++ src/routing/HumanRouter.java
@@ -1,54 +1,44 @@
 package routing;
 
-import core.Connection;
 import core.DTNHost;
 import core.Settings;
-import core.SimClock;
-import movement.RandomWalk;
+import java.util.Random;
+import core.Connection;
 
-
-public class HumanRouter extends ActiveRouter {
-    private double infectionTime = -1;
-    private Settings settings;
+public class HumanRouter extends PassiveRouter {
+    public static final double TURNING_RATE = 1;
 
     public HumanRouter(Settings s) {
         super(s);
-        settings = s;
     }
 
     protected HumanRouter(HumanRouter r) {
         super(r);
-        this.infectionTime = r.infectionTime;
-        this.settings = r.settings;
     }
 
     @Override
-    public void update() {
-        super.update();
-        if (isConnectedToZombie()) {
+    public void changedConnection(Connection con) {
+      if (!con.isUp()) {return;}
 
-            infectionTime = SimClock.getTime();
-            getHost().pauseMovement(1.0); // Pause for 1 second
-            getHost().setName("Zombie (Ex: " + getHost().getInitialName() + ")");
-            getHost().setRouter(new ZombieRouter(settings));
-
+        DTNHost zombie = con.getOtherNode(getHost());
+        MessageRouter zombieRouter = zombie.getRouter();
+        if ( zombieRouter instanceof ZombieRouter) {
+            System.out.println("Connected to Zombie: " + zombie.getName());
+            Random rng = new Random();
+            double r = rng.nextDouble();
+            if (r < TURNING_RATE) {
+              getHost().pauseMovement(300);
+              zombie.pauseMovement(50);
+              getHost().setName("Zombie (Ex: " + getHost().getInitialName() + ")");
+              getHost().setRouter(zombieRouter.replicate());
+              return;
+            }
+            getHost().pauseMovement(50);
+            zombie.pauseMovement(300);
         }
     }
-
     @Override
     public MessageRouter replicate() {
         return new HumanRouter(this);
-    }
-
-    public boolean isConnectedToZombie() {
-
-        for (core.Connection con : getHost().getConnections()) {
-            DTNHost other = con.getOtherNode(getHost());
-            if (other.getRouter() instanceof ZombieRouter) {
-                System.out.println("Connected to Zombie: " + other.getName());
-                return true;
-            }
-        }
-        return false;
     }
 }
--- src/routing/ZombieRouter.java
+++ src/routing/ZombieRouter.java
@@ -2,7 +2,7 @@ package routing;
 
 import core.*;
 
-public class ZombieRouter extends ActiveRouter {
+public class ZombieRouter extends PassiveRouter{
 
     public ZombieRouter(Settings s) {
         super(s);
@@ -10,26 +10,6 @@ public class ZombieRouter extends ActiveRouter {
 
     protected ZombieRouter(ZombieRouter r) {
         super(r);
-    }
-
-    @Override
-    public void update() {
-        super.update();
-        if( isConnectedToHuman() ) {
-            getHost().pauseMovement(1.0);
-        }
-
-    }
-
-    public boolean isConnectedToHuman() {
-
-        for (core.Connection con : getHost().getConnections()) {
-            DTNHost other = con.getOtherNode(getHost());
-            if (other.getRouter() instanceof HumanRouter) {
-                return true;
-            }
-        }
-        return false;
     }
 
     @Override
