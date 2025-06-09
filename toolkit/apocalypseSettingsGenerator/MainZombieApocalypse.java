bb7fdbcd35f53aa13c1ec45605506aa5cf0f3926
neelm <neel.mandal@yahoo.com>
2025-06-09 17:20:43 +0200

    Fixed bugs for generation for Events and entrances and exits

--- toolkit/apocalypseSettingsGenerator/MainZombieApocalypse.java
+++ toolkit/apocalypseSettingsGenerator/MainZombieApocalypse.java
@@ -13,15 +13,25 @@ public class MainZombieApocalypse {
 
 
     //Function to create a settings file for a specific node
-    private static void createSettingsFile(String name, double RoomSizeX, double RoomSizeY, int nrOfHumans, int nrOfZombies, int simulationTime, Set<RoomEdge> edges, int numberOfRuns) throws IOException {
+    private static void createSettingsFile(String name, double RoomSizeX, double RoomSizeY, int nrOfHumans, int nrOfZombies, int simulationTime, Set<RoomEdge> incomingEdges, Set<RoomEdge> outgoingEdges, int numberOfRuns) throws IOException {
         String entrances = "";
+        String exits = "################################\n" +
+                "# Group 3: Exit\n" +
+                "################################\n" +
+                "# Exit number + e\n";
         String runs = "";
-        for (RoomEdge edge : edges) {
+        String events = "";
+
+        if(!incomingEdges.isEmpty()){
+            events = events.concat("#One event per Entrance\n" + "Events.nrof = " + incomingEdges.size() + "\n");
+        }
+        for (RoomEdge edge : incomingEdges) {
+            int eventCounter = 1;
             // Assuming the edge represents an entrance, you can modify this logic as needed
-           entrances = entrances.concat("################################\n" +
+            entrances = entrances.concat("################################\n" +
                     "# Group 4: Entrance for e" + edge.getId() + "e \n" +
                     "################################\n" +
-                    "#Number should be the edge number in the graph" +
+                    "#Number should be the edge number in the graph \n"  +
                     "Group4.groupID = e" + edge.getId() + "e\n" +
                     "Group4.movementModel = ApocalypseMovement\n" +
                     "Group4.initialMovement = NoMovement\n" +
@@ -33,11 +43,18 @@ public class MainZombieApocalypse {
                     "Group2.speed = 0.5, 1.5\n" +
                     "Group4.nrofHosts = 3\n" +
                     "\n");
+
+            events = events.concat(
+                    "Events"+ eventCounter++ + ".filePath = reports/apocalypse/e" +edge.getId() + "e.binee\n");
         }
-        
+
+        for(RoomEdge edge : outgoingEdges) {
+            exits = exits.concat("Group3.groupID = "+edge.getId()+"e\n");
+        }
+
 
         if(numberOfRuns > 1){
-          runs =  runs.concat("MovementModel.rngSeed = [");
+            runs =  runs.concat("MovementModel.rngSeed = [");
             for (int i = 1; i < numberOfRuns; i++) {
                 runs = runs.concat(i +";");
             }
@@ -100,12 +117,8 @@ public class MainZombieApocalypse {
                 "Group2.speed = 0.5, 1.5\n" +
                 "Group2.nrofHosts = " + nrOfZombies + "\n" +
                 "\n" +
-                "################################\n" +
-                "# Group 3: Exit\n" +
-                "################################\n" +
-                "# Exit number + e\n" +
-                "Group3.groupID = 1e\n" +
-                " #Always stationary\n" +
+                exits +
+                "\n #Always stationary\n" +
                 "Group3.movementModel = StationaryMovement\n" +
                 "Group3.router = PassiveRouter\n" +
                 "Group3.nrofInterfaces = 1\n" +
@@ -130,9 +143,7 @@ public class MainZombieApocalypse {
                 "##############################\n" +
                 "# Infection event handling\n" +
                 "##############################\n" +
-                "#One event per Entrance\n" +
-                "Events.nrof = 1\n" +
-                "Events1.filePath = reports/apocalypse/e1e.binee\n" +
+                events +
                 "\n" +
                 "\n" +
                 "\n" +
@@ -161,41 +172,43 @@ public class MainZombieApocalypse {
                 "GUI.EventLogPanel.nrofEvents = 30\n";
         Files.write(Path.of("apocalypse_settings/" + name + ".txt"), settings.getBytes());
     }
-//Function to print the Graph including all relevant data and its edges
-   private static void printGraph(Graph<RoomNode, RoomEdge> graph) {
-       RoomNode start = graph.vertexSet().iterator().next();
-       DepthFirstIterator<RoomNode, RoomEdge> iterator = new DepthFirstIterator<>(graph, start);
-       while (iterator.hasNext()) {
-           RoomNode node = iterator.next();
-           System.out.println("Processing node: " + node.name +
-                   " (Size: " + node.roomSizeX + "x" + node.roomSizeY +
-                   ", Humans: " + node.nrOfHumans +
-                   ", Zombies: " + node.nrOfZombies +
-                   ", Simulation Time: " + node.simulationTime +
-                   ", Number of Runs: " + node.numberOfRuns + ")");
-           for (RoomEdge edge : graph.edgesOf(node)) {
-               System.out.println("  Connected to edge: " + edge.toString());
-           }
-       }
-   }
-   //Function that uses the generates settings for each node (Room) in the Graph
-   public static void generateSettingsFiles(Graph<RoomNode, RoomEdge> buildingLayout) throws IOException {
+    //Function to print the Graph including all relevant data and its edges
+    private static void printGraph(Graph<RoomNode, RoomEdge> graph) {
+        RoomNode start = graph.vertexSet().iterator().next();
+        DepthFirstIterator<RoomNode, RoomEdge> iterator = new DepthFirstIterator<>(graph, start);
+        while (iterator.hasNext()) {
+            RoomNode node = iterator.next();
+            System.out.println("Processing node: " + node.name +
+                    " (Size: " + node.roomSizeX + "x" + node.roomSizeY +
+                    ", Humans: " + node.nrOfHumans +
+                    ", Zombies: " + node.nrOfZombies +
+                    ", Simulation Time: " + node.simulationTime +
+                    ", Number of Runs: " + node.numberOfRuns + ")");
+            for (RoomEdge edge : graph.outgoingEdgesOf(node)) {
+                System.out.println("  Connected to edge: " + edge.toString() + " with ID: " + edge.getId());
+            }
+        }
+    }
+    //Function that uses the generates settings for each node (Room) in the Graph
+    public static void generateSettingsFiles(Graph<RoomNode, RoomEdge> buildingLayout) throws IOException {
 
-       RoomNode start = buildingLayout.vertexSet().iterator().next();
-       DepthFirstIterator<RoomNode, RoomEdge> iterator = new DepthFirstIterator<>(buildingLayout, start);
-       while (iterator.hasNext()) {
-           
-           RoomNode node = iterator.next();
-           createSettingsFile(node.name, node.roomSizeX, node.roomSizeY, node.nrOfHumans, node.nrOfZombies, node.simulationTime, buildingLayout.edgesOf(node), 
-                   node.numberOfRuns);
+        RoomNode start = buildingLayout.vertexSet().iterator().next();
+        DepthFirstIterator<RoomNode, RoomEdge> iterator = new DepthFirstIterator<>(buildingLayout, start);
+        while (iterator.hasNext()) {
 
-       }
-   }
+            RoomNode node = iterator.next();
+            createSettingsFile(node.name, node.roomSizeX, node.roomSizeY, node.nrOfHumans, node.nrOfZombies, node.simulationTime, buildingLayout.incomingEdgesOf(node), buildingLayout.outgoingEdgesOf(node)
+                    ,node.numberOfRuns);
+
+        }
+    }
 
     public static void main(String[] args) throws IOException {
 
         //Depth Search through the graph and create settings files for each room
         Graph<RoomNode, RoomEdge> buildingLayout = BuildingGraphImporter.importGraphFromCSV("example_graphs/buildingLayout.csv");
-       generateSettingsFiles(buildingLayout);
+        generateSettingsFiles(buildingLayout);
+        //printGraph(buildingLayout);
+
     }
 }
